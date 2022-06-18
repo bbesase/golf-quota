d886c1146c2e5e686ad613281c42b3a10bb863e4
Brent Besase <bbesase51@gmail.com>
2022-06-18 01:41:45 -0400

    Integrating firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import firebase from 'firebase';
+import { getFirestore, collection, query, onSnapshot, getDocs, getDoc, collectionGroup, doc } from 'firebase/firestore';
 
-function App() {
+export default function App() {
+  // const firebaseApp = firebase.getApps[0];
+  // fetch('https://tfd-golf-quota-default-rtdb.firebaseio.com/')
+  //   .then((success) => {
+  //     console.log(success)
+  //   });
+
+  useEffect(() => {
+    const testQuery = query(collection(getFirestore(), 'tfd-golfers'));
+    console.log('test', testQuery);
+
+    onSnapshot(testQuery, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        console.log('change type', change);
+      })
+    });
+
+    const test = async () => {
+      const querySnapshot = await getDocs(collection(getFirestore(), "tfd-golfers"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc}`);
+      });
+    };
+
+    const test2 = async () => {
+      const dataRef = await collection(getFirestore(), "tfd-golfers");
+      const dataDoc = await collectionGroup(getFirestore(), 'tfd-golfers');
+      const data = doc(getFirestore(), 'tfd-golfers', 'firstName');
+      const dataSnap = await getDoc(data);
+
+      console.log('snapppppppppppppp', dataSnap);
+      console.log('datadoc', dataDoc);
+      console.log('data ref', dataRef);
+    }
+
+    test();
+    test2();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {/* {JSON.stringify(firebaseApp.options, null, 2)} */}
         </p>
         <a
           className="App-link"
@@ -22,5 +62,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMI15RD5FlA0IS6_4v2Zeq_fFtBTPMlhM",
+  authDomain: "tfd-golf-quota.firebaseapp.com",
+  databaseURL: "https://tfd-golf-quota-default-rtdb.firebaseio.com",
+  projectId: "tfd-golf-quota",
+  storageBucket: "tfd-golf-quota.appspot.com",
+  messagingSenderId: "988180801478",
+  appId: "1:988180801478:web:34f2256e2ab91d6ef674c7",
+  measurementId: "G-X54V321YDY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
