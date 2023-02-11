091ddba28e21954ead6bea761ee584f1a9b46ba1
Brent Besase <bbesase51@gmail.com>
2023-02-11 14:42:20 -0500

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,66 +4,93 @@ import './App.css';
 // import firebase from 'firebase';
 import { getFirestore, collection, query, onSnapshot, getDocs, getDoc, collectionGroup, doc } from 'firebase/firestore';
 import SignIn from "./pages/Auth/Sign-in";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import Homepage from "./pages/Homepage/Home";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignIn />,
+  },
+  {
+    path: '/home',
+    element: <Homepage />
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
 
 export default function App() {
-  // const firebaseApp = firebase.getApps[0];
-  // fetch('https://tfd-golf-quota-default-rtdb.firebaseio.com/')
-  //   .then((success) => {
-  //     console.log(success)
-  //   });
 
-  useEffect(() => {
-    const testQuery = query(collection(getFirestore(), 'tfd-golfers'));
-    console.log('test', testQuery);
-
-    onSnapshot(testQuery, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        console.log('change type', change);
-      })
-    });
-
-    // THIS IS THE ONE
-    const test = async () => {
-      const querySnapshot = await getDocs(collection(getFirestore(), "tfd-golfers"));
-      querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data()}`);
-        const foo = doc.data();
-        console.log('document', doc, foo)
-      });
-    };
-
-    const test2 = async () => {
-      const dataRef = await collection(getFirestore(), "tfd-golfers");
-      const dataDoc = await collectionGroup(getFirestore(), 'tfd-golfers');
-      const data = doc(getFirestore(), 'tfd-golfers', 'firstName');
-      const dataSnap = await getDoc(data);
-
-      console.log('snapppppppppppppp', dataSnap);
-      console.log('datadoc', dataDoc);
-      console.log('data ref', dataRef);
-    }
-
-    test();
-    test2();
-  }, [])
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* {JSON.stringify(firebaseApp.options, null, 2)} */}
-          <SignIn />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+
+// export default function App() {
+//   // const firebaseApp = firebase.getApps[0];
+//   // fetch('https://tfd-golf-quota-default-rtdb.firebaseio.com/')
+//   //   .then((success) => {
+//   //     console.log(success)
+//   //   });
+
+//   useEffect(() => {
+//     const testQuery = query(collection(getFirestore(), 'tfd-golfers'));
+//     console.log('test', testQuery);
+
+//     onSnapshot(testQuery, (snapshot) => {
+//       snapshot.docChanges().forEach((change) => {
+//         console.log('change type', change);
+//       })
+//     });
+
+//     // THIS IS THE ONE
+//     const test = async () => {
+//       const querySnapshot = await getDocs(collection(getFirestore(), "tfd-golfers"));
+//       querySnapshot.forEach((doc) => {
+//         // console.log(`${doc.id} => ${doc.data()}`);
+//         const foo = doc.data();
+//         console.log('document', doc, foo)
+//       });
+//     };
+
+//     const test2 = async () => {
+//       const dataRef = await collection(getFirestore(), "tfd-golfers");
+//       const dataDoc = await collectionGroup(getFirestore(), 'tfd-golfers');
+//       const data = doc(getFirestore(), 'tfd-golfers', 'firstName');
+//       const dataSnap = await getDoc(data);
+
+//       console.log('snapppppppppppppp', dataSnap);
+//       console.log('datadoc', dataDoc);
+//       console.log('data ref', dataRef);
+//     }
+
+//     test();
+//     test2();
+//   }, [])
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           {/* {JSON.stringify(firebaseApp.options, null, 2)} */}
+//           <SignIn />
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
