72e5ea2bc7ffab8514d21b07a02425a29a50b83b
Brent Besase <bbesase51@gmail.com>
2023-05-13 13:54:22 -0400

    Added ability to add a new quota

--- src/pages/GolferDashboard/Dashboard.tsx
+++ src/pages/GolferDashboard/Dashboard.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Golfer } from 'src/types/golfer';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, updateDoc, doc, } from 'firebase/firestore';
 import { useParams } from 'react-router-dom';
-import { Grid, GridItem, Heading, Input, Text } from '@chakra-ui/react';
+import { Button, Grid, GridItem, Heading, Input, Text, NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper } from '@chakra-ui/react';
+import { app } from 'src';
 
 export default function Dashboard(props: any) {
   const [golfer, setGolfer] = useState<Golfer | null>(null);
+  const [newQuota, setNewQuota] = useState<number>(0)
   const { id } = useParams();
+  const db = getFirestore(app);
 
   useEffect(() => {
     const getGolferInfo = async () => {
@@ -22,10 +29,33 @@ export default function Dashboard(props: any) {
     };
     
     getGolferInfo();
-  }, []);
+  }, [golfer]);
 
-  const handleQuotaChange = (label: string) => {
 
+  const handleQuotaChange = async () => {
+    if (golfer && newQuota) {
+      golfer.quota6 = golfer.quota5;
+      golfer.quota5 = golfer.quota4;
+      golfer.quota4 = golfer.quota3;
+      golfer.quota3 = golfer.quota2;
+      golfer.quota2 = golfer.quota1;
+      golfer.quota1 = Number(newQuota);
+      golfer.average = Math.floor(( golfer.quota1 + golfer.quota2 + golfer.quota3 + golfer.quota4 + golfer.quota5 + golfer.quota6 ) / 6)
+    }
+    const docRef = doc(db, `tfd-golfers/${id}`);
+
+    await updateDoc(docRef, {
+      quota1: golfer?.quota1,
+      quota2: golfer?.quota2,
+      quota3: golfer?.quota3,
+      quota4: golfer?.quota4,
+      quota5: golfer?.quota5,
+      quota6: golfer?.quota6,
+      average: golfer?.average,
+    }).then((response: any)   =>  {
+      console.log('response', response)
+      setGolfer(golfer);
+    })
   }
 
   return (
@@ -36,19 +66,92 @@ export default function Dashboard(props: any) {
       gap={4}
       p={8}
     >
-      <GridItem rowSpan={2} colSpan={1} bg='tomato'>
+      <GridItem rowSpan={2} colSpan={1} p={24} bg='tomato'>
         <Heading p={12}>{`${golfer?.firstName} ${golfer?.lastName}`}</Heading>
         <Heading p={12} mt={4}>Average Quota: {golfer?.average}</Heading>
+        {golfer?.auth && (
+          <div>
+            <Text fontSize='lg'>New Quota</Text>
+            <NumberInput
+              defaultValue={newQuota}
+              value={newQuota}
+              min={0} 
+              max={100} 
+              step={1} 
+              mt={4} 
+              onChange={value => setNewQuota(Number(value))}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Button 
+              colorScheme='teal'
+              size='lg'
+              onClick={handleQuotaChange}
+              mt={4}
+            >
+              Submit Quota
+            </Button>
+          </div>
+        )}
       </GridItem>
       <GridItem colSpan={1} p={24} bg='paleturquoise'>
         <Text fontSize='lg'>Quota 1</Text>
-        <Input size='lg' mt={1} value={golfer?.quota1} disabled={golfer?.auth === false} onChange={() => handleQuotaChange('quota1')}/>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota1 || ''} 
+          disabled={true} 
+        />
       </GridItem>
-      <GridItem colSpan={1} pl='2' bg='paleturquoise' />
-      <GridItem colSpan={1} pl='2' bg='paleturquoise' />
-      <GridItem colSpan={1} pl='2' bg='paleturquoise' />
-      <GridItem colSpan={1} pl='2' bg='paleturquoise' />
-      <GridItem colSpan={1} pl='2' bg='paleturquoise' />
+      <GridItem colSpan={1} p={24} bg='paleturquoise'>
+        <Text fontSize='lg'>Quota 2</Text>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota2 || ''} 
+          disabled={true} 
+        />
+      </GridItem>
+      <GridItem colSpan={1} p={24} bg='paleturquoise'>
+        <Text fontSize='lg'>Quota 3</Text>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota3 || ''} 
+          disabled={true} 
+        />
+      </GridItem>
+      <GridItem colSpan={1} p={24} bg='paleturquoise'>
+        <Text fontSize='lg'>Quota 4</Text>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota4 || ''} 
+          disabled={true} 
+        />
+      </GridItem>
+      <GridItem colSpan={1} p={24} bg='paleturquoise'>
+        <Text fontSize='lg'>Quota 5</Text>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota5 || ''} 
+          disabled={true} 
+        />
+      </GridItem>
+      <GridItem colSpan={1} p={24} bg='paleturquoise'>
+        <Text fontSize='lg'>Quota 6</Text>
+        <Input 
+          size='lg' 
+          mt={1} 
+          value={golfer?.quota6 || ''} 
+          disabled={true} 
+        />
+      </GridItem>
     </Grid>
   )
 }
